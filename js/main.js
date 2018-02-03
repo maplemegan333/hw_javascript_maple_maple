1b1d8cd50067edf5029ac8f039711c229dc40d9b
maplemegan333 <maplemegan@ufl.edu>
2018-02-03 14:46:36 -0500

    Add presidents JavaScript

--- js/main.js
+++ js/main.js
@@ -5,3 +5,13 @@ var iceCream = [
 ];
 
 iceCream.push('green tea');
+
+var presidents = [
+    {firstName: 'Donald', lastName: 'Trump', termLength: 'One Year', party: 'Republican', yearsOfPresidency: '2017-2018'},
+    {firstName: 'Obama', lastName: 'Barack', termLength: 'Eight Years', party: 'Democrat', yearsOfPresidency: '2009-2017'},
+    {firstName: 'George W.', lastName: 'Bush', termLength: 'Eight Years', party: 'Republican', yearsOfPresidency: '2001-2009'},
+    {firstName: 'Bill', lastName: 'Clinton', termLength: 'Eight Years', party: 'Democrat', yearsOfPresidency: '1993-2001'},
+    {firstName: 'George H. W.', lastName: 'Bush', termLength: 'Four Years', party: 'Republican', yearsOfPresidency: '1989-1993'}
+];
+
+console.log(presidents[2].firstName + ' ' + presidents[2].lastName);
