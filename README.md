8ca402b408812e32331a4590c3dbac2e5f23cc23
maplemegan333 <maplemegan@ufl.edu>
2018-02-04 15:36:48 -0500

    Finalize project and create README file

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
+# JavaScript Assignment
+* Megan Maple
+* February 4th, 2018
+* [Assignment can be found here.](https://github.com/maplemegan333/hw_javascript_maple_maple)
 
+## Resources
+I used the below resources for my project.
+
+* [Editorconfig](http://editorconfig.org/)
+    * This was used to create my .editoryconfig file.
+* [W3C Schools - JavaScript Array Push Method](https://www.w3schools.com/jsref/jsref_push.asp)
+    * I used this information to find the best method of adding to my iceCream array.
+
+## Deductions
+I reviewed the list of deductions for this assignment and in the syllabus.
+
+## Comments
+I am better at JavaScript than I thought!
+
+The last problem took a long time to figure out!
--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
+// Simple array
+
 var iceCream = [
     'chocolate peanut butter',
     'raspberry sorbet',
     'vanilla'
 ];
 
+// Adds additional item to iceCream array
+
 iceCream.push('green tea');
+
+// Array containing objects with various properties that hold pertinate information about various presidents
 
 var presidents = [
     {firstName: 'Donald', lastName: 'Trump', termLength: 'One Year', party: 'Republican', yearsOfPresidency: '2017-2018'},
@@ -14,14 +20,25 @@ var presidents = [
     {firstName: 'George H. W.', lastName: 'Bush', termLength: 'Four Years', party: 'Republican', yearsOfPresidency: '1989-1993'}
 ];
 
+// Instructs console to log a specific president name
+
 console.log(presidents[2].firstName + ' ' + presidents[2].lastName);
+
+// Function declaration with 3 parameters
 
 function howdy(first, last, message) {
     return first + ' ' + last + ' says ' + message;
 }
 
+// Varriable message assigning my first name, last name and a message to howdy
+
 var message = howdy('Megan', 'Maple', 'Hello everyone! How are you today?');
+
+// Loging message in the console
+
 console.log(message);
+
+// Function named numba stipulating 3 seperate messages based on varous string lengths
 
 var numba = function (string) {
     if (string.length < 7) {
@@ -33,18 +50,26 @@ var numba = function (string) {
     }
 };
 
+// Instructing the console to run the numba function with three specific strings
+
 numba('7');
 
 numba('seventy');
 
 numba('university');
 
+// Function insception calls function passed in first parameter using second parameter
+
 function inception(display, faveMovie) {
     display(faveMovie);
 }
 
+// Instructs console to log parameter plus string
+
 var printMovie = function (movieTitle) {
     console.log(movieTitle + ' is a fantastic movie.');
 };
+
+// calls insception using function printMovie as the first parameter and a string "Howl's Moving Castle"
 
 inception(printMovie, "Howl's moving Castle");
